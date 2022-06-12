72f956c1b9ce7486bfa886f5fd355b2b8240921d
matovu-farid <matovu90@gmail.com>
2022-06-12 19:32:10 +0300

    Add ability to edit a project

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from 'react'
 import { useAppDispatch } from '../app/hooks';
 import { deleteAProject, updateAProject } from '../app/projects';
 import { Project } from '../interfaces/project';
@@ -11,23 +11,41 @@ const Options = (props: { project: Project}) => {
     dispatch(deleteAProject(project))
   }
   const onUpdate = ()=>{
-    dispatch(updateAProject(project))
+    const newProject = {
+      id: project.id,
+      name: name,
+      description: description,
+      image: image,
+      github: github
+
+    }
+    dispatch(updateAProject(newProject))
     toggleDialog()
   }
   const [open,setOpen] = useState(false)
   const toggleDialog = ()=>{
     setOpen(!open)
   }
+  
+  const [name, setName] = useState(project.name)
+  const [description, setDescription] = useState(project.description)
+  const [image, setImage] = useState(project.image)
+  const [github, setGithub] = useState(project.github)
  
   return (
     <div className='flex gap-1'>
-      <dialog className='shadow-lg' open={open}>
-        <div className='w-full h-full flex flex-col p-3 text-start justify-start'>
-          <label  htmlFor="title" className='font-bold'>Name</label>
-          <input type="text" id="name" defaultValue={project.name} />
-          <label htmlFor="description" className='font-bold'>Description</label>
-          <textarea id='description' defaultValue={project.description}   cols={30} rows={10}></textarea>
+      <dialog  className='shadow-lg' open={open}>
+        <form className='w-full h-full flex flex-col p-3 text-start justify-start gap-2'>
+        <input type="text" id="name" placeholder='title' name="name" value={name} onChange={(e)=>setName((e.target as HTMLInputElement).value)} />
+
+          <textarea id='description' placeholder='description' name='description' value={description}   cols={30} rows={10} onChange={(e)=>setDescription((e.target as HTMLTextAreaElement).value)}></textarea>
+        <div className=' flex flex-col'>
+          <p className='bold'>Links</p>
+
+         <input type="text" id="image" placeholder='image' name="image" defaultValue={image} onChange={(e)=>setImage((e.target as HTMLInputElement).value)} />
+          <input type="text" id="github" placeholder='github' name="github" defaultValue={github} onChange={(e)=>setGithub((e.target as HTMLInputElement).value)} />
         </div>
+        </form>
         <div className='flex gap-2 justify-end'>
           <Button text='Update' onClick={onUpdate}></Button>
           <Button text='Cancel' onClick={toggleDialog}></Button>
