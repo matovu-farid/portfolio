da713b7f2d899319afa4dd7449c1d8d4a7e22671
matovu-farid <matovu90@gmail.com>
2022-06-14 01:29:44 +0300

    Add blur in animation

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,7 +5,7 @@ import ProjectComponent from '../components/Project'
 const Projects = () => {
   const projects = useAppSelector(state=> state.projects.all)
   return (
-    <div className='flex flex-col gap-2 mx-auto max-w-lg'>
+    <div className='flex flex-col gap-2 mx-auto max-w-lg animate-entrance'>
       {
         projects.map(project =><ProjectComponent key={project.id} project={project}/>)
       }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    keyframes: {
+      entrance: {
+        '0%': {      
+          filter: 'blur(0.5rem)',
+          tranform: 'scale(0.5)'
+        },
+        '50%': {
+          filter: 'blur(0.25rem)',
+          tranform: 'scale(0.75)'
+        },
+        '100%': {
+          filter: 'blur(0)',
+          tranform: 'scale(1)'
+        }
+      }
+    },
+    animation: {
+      'entrance' : 'entrance 4s ease-in 1'
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
