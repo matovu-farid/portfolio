ab1e28068ebf1879ec2e812d14b53a15951ef566
matovu-farid <matovu90@gmail.com>
2022-06-15 15:52:32 +0300

    Refactor sidebar to use the constants and add the skills and add skill links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,15 +4,18 @@ import SideLink from './SideLink'
 import { AiFillHome } from 'react-icons/ai';
 import { MdFavorite } from 'react-icons/md';
 import { GoDiffAdded } from 'react-icons/go';
+import { ADDPROJECTROUTE, ADDSKILLROUTE, FAVOURITESROUTE, HOMEROUTE, SKILLSROUTE } from '../helpers/constants';
 
 
 const Sidebar = () => {
   
   return (
     <div className='h-full min-h-screen bg-slate-900 text-white flex flex-col pt-24 text-start'>
-      <SideLink icon={<AiFillHome/>} to='/' text='Home'></SideLink>
-      <SideLink icon={<MdFavorite/>} to='/favorites' text='Favourites'></SideLink>
-      <SideLink icon={<GoDiffAdded/>} to='/addproject' text='Add Project'></SideLink>
+      <SideLink icon={<AiFillHome/>} to={HOMEROUTE} text='Home'></SideLink>
+      <SideLink icon={<MdFavorite/>} to={FAVOURITESROUTE} text='Favourites'></SideLink>
+      <SideLink icon={<GoDiffAdded/>} to={ADDPROJECTROUTE} text='Add Project'></SideLink>
+      <SideLink icon={<GoDiffAdded/>} to={SKILLSROUTE} text='Skills'></SideLink>
+      <SideLink icon={<GoDiffAdded/>} to={ADDSKILLROUTE} text='Add Skills'></SideLink>
     </div>
   )
 }
