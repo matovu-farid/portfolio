95eff1296348ff28f74beca84500bd2e65934f32
matovu-farid <matovu90@gmail.com>
2022-06-26 06:01:09 +0300

    Add resume interface

--- src/interfaces/resume.ts
+++ src/interfaces/resume.ts
@@ -1,8 +1,33 @@
 
+export interface History {
+  job: String,
+  date: String,
+  points: String[],
+  location: String
+}
+export interface Education {
+  institution: String,
+  date: String,
+  title: String,
+}
+
+export interface RSkill {
+  frontEnd:String[],
+  backEnd:String[],
+  tools: String[]
+
+}
+export interface RProject {
+  title: String,
+  points: String[]
+}
 export interface Resume {
   id: string,
   name: string,
-  description: string,
-  github: string,
-  image: string,
+  title: String,
+  intro: String,
+  links: string[],
+  history: History[],
+  skills: RSkill[],
+  education: Education[]
 }
